ca6a32cd976a4505fb22d89a83b4dafafb3dd433
tabhresh <tabhresh@gmail.com>
2019-05-03 10:16:07 +0530

    3rd May

--- src/module2/Facebook.java
+++ src/module2/Facebook.java
@@ -7,47 +7,42 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Facebook {
 
 	public static void main(String[] args) {
-		
-		//Open Browser
+
+		// Open Browser
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium\\drivers\\chromedriver_2.46.exe");
 		WebDriver driver = new ChromeDriver();
-		
-		//Open AUT
+
+		// Open AUT
 		driver.get("https://www.facebook.com/");
-		
-		/*
-		 //Using ID 
-		  driver.findElement(By.id("u_0_j")).sendKeys("Abhresh");
-		  driver.findElement(By.id("u_0_l")).sendKeys("Sugandhi");
-		
-		//Using name
+
+		// Using ID
+		driver.findElement(By.id("u_0_j")).sendKeys("Abhresh");
+		driver.findElement(By.id("u_0_l")).sendKeys("Sugandhi");
+
+		// Using name
 		driver.findElement(By.name("firstname")).sendKeys("Abhresh");
 		driver.findElement(By.name("lastname")).sendKeys("Sugandhi");
 		driver.findElement(By.name("reg_email__")).sendKeys("0987654321");
-		
-		//Using Classname
+
+		// Using Classname
 		driver.findElement(By.className("inputtext")).sendKeys("Abhresh");
 		driver.findElement(By.className("inputtext")).sendKeys("Sugandhi");
-		 */
-		
-		
-		//Using LinkText
-		driver.findElement(By.linkText("Forgotten account?")).click();
-		
-		//driver.navigate().back();
-		
-		//Using PartialLinkText
-		driver.findElement(By.partialLinkText("Forgot")).click();
-		
-		/*
-		 * driver.navigate().back();
-		 * 
-		 * driver.navigate().forward();
-		 */		
-		//Using CSS
-		//Using TagName
-		//Using Xpath
 
+		// Using LinkText
+		driver.findElement(By.linkText("Forgotten account?")).click();
+
+		driver.navigate().back();
+
+		// Using PartialLinkText
+		driver.findElement(By.partialLinkText("Forgot")).click();
+
+		driver.navigate().back();
+
+		driver.navigate().forward();
+
+		// Using CSS
+		// Using TagName
+		// Using Xpath
 
 	}
 
