19042ccb1bf4d6bc9a651d6e6a79b637652428f4
tabhresh <tabhresh@gmail.com>
2019-05-13 10:56:05 +0530

    13th May

---
+++ src/module6/TestNG.java
@@ -0,0 +1,53 @@
+package module6;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestNG {
+
+	WebDriver driver;
+
+	@Test
+	public void OpenBrowser() {
+
+		System.setProperty("webdriver.chrome.driver", "E:\\Abhresh\\Installation_stuff\\ExeFiles\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+	}
+
+	@Test
+	public void OpenAUT() {
+		driver.get("http://newtours.demoaut.com/");
+	}
+
+	@Test
+	public void Login() throws InterruptedException {
+		// Enter UserID
+		driver.findElement(By.name("abh")).sendKeys("tutorial");
+
+		// Enter Password
+		driver.findElement(By.name("password")).sendKeys("tutorial");
+
+		// Click on Sign in button
+		driver.findElement(By.name("login")).click();
+		Thread.sleep(3000);
+	}
+
+	@Test
+	public void Logout() throws InterruptedException {
+		// Click on Sign off
+		driver.findElement(By.linkText("SIGN-OFF")).click();
+		Thread.sleep(3000);
+	}
+
+	@Test
+	public void CloseBrowser() {
+		driver.close();
+	}
+}
